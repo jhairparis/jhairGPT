219dba57908dd56fd851acd34a6b2fd16828ceb9
Jhair Paris <70655025+jhairparis@users.noreply.github.com>
2025-02-17 11:40:15 -0500

    Refactor layout and page components to integrate sidebar and history features with hydration support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,8 @@ import "./globals.css";
 import Provider from "@/features/shared/providers/provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import AppSidebar from "@/features/shared/components/sidebar";
 import { cookies } from "next/headers";
 import { SIDEBAR_COOKIE_NAME } from "@/features/shared/constants/sidebar";
-import History from "@/features/shared/components/history";
-import { getQueryClientDynamic } from "@/features/shared/lib/queryClientDynamic";
-import { ConversationsServer } from "@/features/chat-interface/utils/chat-queries";
-import { getAuth } from "@/features/auth/utils/auth";
-import Speak from "@/features/chat-interface/components/Speak";
-import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ["latin"], display: "swap" });
 
@@ -25,26 +18,13 @@ type RootLayoutProps = Readonly<{
 
 export default async function RootLayout({ children }: RootLayoutProps) {
   const cookieStore = cookies();
-  const queryClient = getQueryClientDynamic();
-
   const defaultOpen = cookieStore.get(SIDEBAR_COOKIE_NAME)?.value === "true";
-
-  const { authCookies } = await getAuth(cookieStore);
-
-  await queryClient.prefetchQuery(ConversationsServer(authCookies));
 
   return (
     <html lang="en">
       <body className={inter.className} suppressHydrationWarning>
         <Provider SideBarDefault={defaultOpen}>
-          <div className="flex min-h-dvh w-full relative">
-            <AppSidebar>
-              <HydrationBoundary state={dehydrate(queryClient)}>
-                <History />
-              </HydrationBoundary>
-            </AppSidebar>
-            <Speak>{children}</Speak>
-          </div>
+          <div className="flex min-h-dvh w-full relative">{children}</div>
         </Provider>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,42 @@
+import Speak from "@/features/chat-interface/components/Speak";
+import AppSidebar from "@/features/shared/components/sidebar";
+import History from "@/features/shared/components/history";
+import { cookies } from "next/headers";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { getQueryClientDynamic } from "@/features/shared/lib/queryClientDynamic";
+import { ConversationsServer } from "@/features/chat-interface/utils/chat-queries";
+import { getAuth } from "@/features/auth/utils/auth";
+
 const appName = "JhairGPT";
 
-const Page = () => {
+const Page = async () => {
+  const cookieStore = cookies();
+  const queryClient = getQueryClientDynamic();
+
+  const { authCookies } = await getAuth(cookieStore);
+
+  await queryClient.prefetchQuery(ConversationsServer(authCookies));
+
   return (
-    <main className="flex-grow flex flex-col justify-center">
-      <div className="flex flex-col justify-center items-center max-w-4xl w-full text-center mx-auto px-4 sm:px-6 lg:px-8">
-        <h1 className="text-3xl font-bold text-gray-800 sm:text-4xl dark:text-white m-0 max-w-fit overflow-hidden whitespace-nowrap animation">
-          Welcome to{` ${appName}`}
-        </h1>
-        <p className="mt-3 text-gray-600 dark:text-gray-400">
-          Your copilot for all
-        </p>
-      </div>
-    </main>
+    <>
+      <AppSidebar>
+        <HydrationBoundary state={dehydrate(queryClient)}>
+          <History />
+        </HydrationBoundary>
+      </AppSidebar>
+      <Speak>
+        <main className="flex-grow flex flex-col justify-center">
+          <div className="flex flex-col justify-center items-center max-w-4xl w-full text-center mx-auto px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-800 sm:text-4xl dark:text-white m-0 max-w-fit overflow-hidden whitespace-nowrap animation">
+              Welcome to{` ${appName}`}
+            </h1>
+            <p className="mt-3 text-gray-600 dark:text-gray-400">
+              Your copilot for all
+            </p>
+          </div>
+        </main>
+      </Speak>
+    </>
   );
 };
 
